c91df515d746523c48065a1eba57b981eb7657d6
yahya bnoun <yahyaiko22@gmail.com>
2024-08-03 16:31:01 +0100

    hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,28 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu")
+const navToggle = document.getElementById("nav-toggle")
+const navClass = document.getElementById("nav-close") 
 
+if (navToggle) {
+    navToggle.addEventListener("click",()=>{
+        navMenu.classList.add("show-menu")
+    })
+}
 
+if (navClass) {
+    navClass.addEventListener("click",()=>{
+        navMenu.classList.remove("show-menu")
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll(".nav__link") 
+
+const linkAction = ()=> {
+    navMenu.classList.remove("show-menu")
+} 
+
+navLink.forEach(n => n.addEventListener("click",linkAction))
 
 /*=============== SHADOW HEADER ===============*/
 
